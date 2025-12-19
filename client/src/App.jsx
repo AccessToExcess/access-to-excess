b95635cea53e3cae0da2034a100b6e51468af1dd
skylarsoon <68980693+skylarsoon@users.noreply.github.com>
2025-12-19 12:24:18 -0800

    Removed tanstack table, made custom cards instead

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,4 @@
-import Table from './components/Table.jsx'
-
-const foodPickupColumns = [
-    {
-        accessorKey: 'fields.Location',
-        header: 'Location'
-    },
-    {
-        accessorKey: 'fields.Address',
-        header: 'Address',
-    },
-    {
-        accessorKey: 'fields.StartTime',
-        header: 'StartTime',
-        cell: info => new Date(info.getValue()).toLocaleString(),
-    },
-    {
-        accessorKey: 'fields.EndTime',
-        header: 'EndTime',
-        cell: info => new Date(info.getValue()).toLocaleString(),
-},
-    {
-        accessorKey: 'fields.Notes',
-        header: 'Notes',
-    },
-]; 
-
+import Table from './components/FoodPickupTable.jsx'
 
 function App() {
     return (
@@ -32,7 +6,6 @@ function App() {
             <h1 className="text-2xl font-bold">Access to Excess</h1>
             <Table 
                 endpoint="food-pickup"
-                columns={foodPickupColumns}
                 title="Food Pickup"
             />
         </div>
